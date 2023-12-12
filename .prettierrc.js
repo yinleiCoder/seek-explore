d5bd2d043b3a9d81a2c0bd44a11cd60c0f7e684c
yinlei <1099129793@qq.com>
2023-12-12 22:03:17 +0800

    chore: 添加husky、commit-lint

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  endOfLine: 'auto',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  printWidth: 100,
+  proseWrap: 'preserve',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  parser: 'typescript',
+}
