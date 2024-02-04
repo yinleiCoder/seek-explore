98ffa1780677815503d1d3ddd1764151f76c7f3c
yinlei <1099129793@qq.com>
2024-02-04 11:29:57 +0800

    chore: 修改留言墙UI

--- app/wall/page.tsx
+++ app/wall/page.tsx
@@ -1,24 +1,33 @@
-import { Metadata } from 'next'
-import { AudioPlayer } from '@/components/audioPlayer'
-import Comment from '@/components/comment'
-import { Suspense } from 'react'
+'use client'
 
-export const metadata: Metadata = {
-  title: '留言墙',
-  description: '留下你最想说的话吧，10年，20年...这里依然保存着你当初留下的话',
-}
+import { Suspense, useEffect } from 'react'
+import Lenis from '@studio-freight/lenis'
+import { AudioPlayer } from '@/components/audioPlayer'
+import ZoomParallax from '@/components/zoomParallax'
+import Comment from '@/components/comment'
 
 export default function WallPage() {
+  useEffect(() => {
+    const lenis = new Lenis()
+    function raf(time: any) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+  }, [])
   return (
-    <div className="min-h-[calc(100vh-56px)] flex flex-col p-2 md:p-0 gap-5 lg:container lg:max-w-3xl mx-auto">
-      <section className="w-full">
-        <AudioPlayer track="/audios/thingsYouSaid.m4a" />
-      </section>
-      <section className="w-full mt-2">
-        <Suspense fallback={<p>Loading...</p>}>
-          <Comment placeholder="这里是属于你的天空🤔" />
-        </Suspense>
-      </section>
-    </div>
+    <main className="w-full">
+      <ZoomParallax />
+      <div className="flex flex-col p-2 my-2 md:p-0 gap-5 lg:container lg:max-w-3xl mx-auto">
+        <section className="w-full">
+          <AudioPlayer track="/audios/thingsYouSaid.m4a" />
+        </section>
+        <section className="w-full mt-2">
+          <Suspense fallback={<p>Loading...</p>}>
+            <Comment placeholder="这里是属于你的天空🤔" />
+          </Suspense>
+        </section>
+      </div>
+    </main>
   )
 }
---
+++ components/zoomParallax/index.tsx
@@ -0,0 +1,125 @@
+'use client'
+
+import React, { useRef } from 'react'
+import Image from 'next/image'
+import { useScroll, useTransform, motion } from 'framer-motion'
+import Wall1 from '../../public/images/wall/wall1.webp'
+import Wall2 from '../../public/images/wall/wall2.webp'
+import Wall3 from '../../public/images/wall/wall3.webp'
+import Wall4 from '../../public/images/wall/wall4.webp'
+import Wall5 from '../../public/images/wall/wall5.webp'
+import Wall6 from '../../public/images/wall/wall6.webp'
+import Wall7 from '../../public/images/wall/wall7.webp'
+
+/**
+ * 不动画img的父元素，而是动画img的父元素的父元素，这样就可以解决img
+ */
+function ZoomParallax() {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ['start start', 'end end'],
+  })
+  const scale4 = useTransform(scrollYProgress, [0, 1], [1, 4])
+  const scale5 = useTransform(scrollYProgress, [0, 1], [1, 5])
+  const scale6 = useTransform(scrollYProgress, [0, 1], [1, 6])
+  const scale7 = useTransform(scrollYProgress, [0, 1], [1, 7])
+  const scale8 = useTransform(scrollYProgress, [0, 1], [1, 8])
+  const scale9 = useTransform(scrollYProgress, [0, 1], [1, 9])
+
+  const pictures = [
+    {
+      src: Wall7,
+      scale: scale4,
+      imageContainerStyle: {},
+    },
+    {
+      src: Wall6,
+      scale: scale5,
+      imageContainerStyle: {
+        top: '-30vh',
+        left: '5vw',
+        width: '35vw',
+        height: '30vh',
+      },
+    },
+    {
+      src: Wall3,
+      scale: scale8,
+      imageContainerStyle: {
+        top: '-10vh',
+        left: '-25vw',
+        width: '20vw',
+        height: '45vh',
+      },
+    },
+    {
+      src: Wall4,
+      scale: scale7,
+      imageContainerStyle: {
+        top: '',
+        left: '27.5vw',
+        width: '25vw',
+        height: '25vh',
+      },
+    },
+    {
+      src: Wall5,
+      scale: scale6,
+      imageContainerStyle: {
+        top: '27.5vh',
+        left: '5vw',
+        width: '20vw',
+        height: '25vh',
+      },
+    },
+    {
+      src: Wall2,
+      scale: scale9,
+      imageContainerStyle: {
+        top: '27.5vh',
+        left: '-22.5vw',
+        width: '30vw',
+        height: '25vh',
+      },
+    },
+    {
+      src: Wall1,
+      scale: scale5,
+      imageContainerStyle: {
+        top: '27.5vh',
+        left: '25vw',
+        width: '15vw',
+        height: '15vh',
+      },
+    },
+  ]
+
+  return (
+    <div className="h-[300vh] relative" ref={containerRef}>
+      <div className="sticky top-0 h-[100vh] bg-white dark:bg-zinc-900 overflow-hidden">
+        {pictures.map(({ scale, src, imageContainerStyle }, index) => {
+          return (
+            <motion.div
+              key={index}
+              style={{ scale }}
+              className="w-full h-full absolute top-0 flex justify-center items-center"
+            >
+              <div style={{ ...imageContainerStyle }} className="w-[25vw] h-[25vh] relative">
+                <Image
+                  src={src}
+                  fill
+                  alt="girl"
+                  placeholder="blur"
+                  className="object-cover rounded-md"
+                />
+              </div>
+            </motion.div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default ZoomParallax
